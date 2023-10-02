8b21ca0f3cf831fad05fd7f40c4c72b31cbcd3d2
TuanNguyen071121 <nguyenvantuan071121.34@gmail.com>
2023-10-02 16:22:15 +0700

    lg si done

--- App.js
+++ App.js
@@ -5,17 +5,21 @@ import { createStackNavigator } from '@react-navigation/stack';
 import ViewImageScreen from './screen/ViewImageScreen';
 import WelcomeScreen from './screen/Welcomescreen';
 import Onboarding from './screen/onboarding';
+import Register from './screen/Register';
+import Login from './screen/login';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    // <NavigationContainer>
-    //   <Stack.Navigator initialRouteName="WelcomeScreen">
-    //     <Stack.Screen name="WelcomeScreen" component={WelcomeScreen} options={{ headerShown: false }} />
-    //     <Stack.Screen name="ViewImageScreen" component={ViewImageScreen} options={{ headerShown: false }} />
-    //   </Stack.Navigator>
-    // </NavigationContainer>
-    <Onboarding/>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='Register' screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Onboarding" component={Onboarding} />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Login" component={Login} />
+        {/* <Stack.Screen name="Home" component={Home} /> */}
+      </Stack.Navigator>
+    </NavigationContainer >
+    // <Onboarding/>
   );
 }
